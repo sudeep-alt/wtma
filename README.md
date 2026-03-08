e2b297393d2fd34757de22b0df5d17425498b39e
Defender <defenderoftruth2010@gmail.com>
2026-03-08 21:34:11 +0530

    first

---
+++ README.md
@@ -0,0 +1,82 @@
+# WTMA: Who touched my app?
+WTMA is an ASGI middleware purposed to log every IP on your web app with detailed info.
+**Version:** 0.1.0
+
+## Features
+- Geolocation logging: Log every IP with their geolocation data, such as country, continent. region and city.
+- Proxy checking: Verify whether IPs were associated with proxies.
+- Hosting checking: Verify whether IPs were associated with hosting and datacenters.
+- Last visit: View the dynamic and exact time when an IP hit your app.
+- Multiple formats: Log data in either JSON or TOML format.
+- Console logging: View brief IP info in the console as well.
+- Framework support: Supports all ASGI frameworks ([FastAPI](https://fastapi.tiangolo.com/), [Starlette](https://starlette.dev/) etc.)
+
+## Quickstart
+### Install the `wtma` python package
+```commandline
+pip install wtma
+```
+### Implement it to your app
+**FastAPI exmaple:**
+```python
+from fastapi import FastAPI
+from wtma import WTMA
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {
+        "message": "Hello, world!"
+    }
+
+# Wrap the app at the end
+app = WTMA(app, log_path="log.json", file_format="JSON", log_to_console=True)
+```
+
+Also, here's the signature of the constructor method:
+```python
+    def __init__(self, app, log_path: str, file_format:Literal["JSON", "TOML"]="JSON", log_to_console:bool=True):
+```
+
+> [!NOTE]
+It's recommended to wrap the app with `WTMA` class at the end instead of using `add_middleware` because [Starlette](https://starlette.dev/),
+which [FastAPI](https://fastapi.tiangolo.com/) is built on top of, suppresses exceptions raised in middleware's initialization, making it difficult to
+debug.
+
+## Log samples
+### JSON
+```json
+{
+  "1.1.1.1": {
+    "country": "Australia",
+    "continent": "Oceania",
+    "city": "South Brisbane",
+    "region": "QLD",
+    "is_proxy": false,
+    "is_hosting": true,
+    "is_mobile": false,
+    "reverseDNS": "one.one.one.one",
+    "last_seen": "2026-03-08 15:54:05.067613+00:00"
+  }
+}
+```
+
+### TOML
+```toml
+["1.1.1.1"]
+country = "Australia"
+continent = "Oceania"
+city = "South Brisbane"
+region = "QLD"
+is_proxy = false
+is_hosting = true
+is_mobile = false
+reverseDNS = "one.one.one.one"
+last_seen = "2026-03-08 15:54:05.067613+00:00"
+```
+
+## Data source
+WTMA uses [ip-api.com](https://ip-api.com/) as its data source. The data returned may contain errors or be inaccurate.
+
+If you found this project useful, please consider giving it a star!
---
+++ src/wtma/main.py
@@ -0,0 +1,203 @@
+import tomllib
+from pathlib import Path
+from beartype import beartype
+from typing import Literal
+import httpx
+import json
+from datetime import datetime, UTC
+import logging
+import tomli_w
+
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+# Disable HTTPX logging
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+
+class WTMA:
+    @beartype
+    def __init__(self, app, log_path: str, file_format:Literal["JSON", "TOML"]="JSON", log_to_console:bool=True):
+        self.app = app
+        self.log_path = log_path
+        self.file_format = file_format.upper()
+        self.log_to_console = log_to_console
+
+    async def __call__(self, scope, receive, send):
+
+        ip = scope["client"][0]
+        data = {}
+
+        # JSON FORMAT HANDLING
+        if self.file_format == "JSON":
+            # Check if the JSON log file already exists and already stores the data for the IP, if so, only update the time
+            if Path(self.log_path).exists() and Path(self.log_path).is_file():
+                with open(self.log_path, "r") as f:
+                    try:
+                        data = json.load(f)
+                    except json.decoder.JSONDecodeError:
+                        if f.read():
+                            raise Exception(f"The currently existing file {self.log_path} does not contain valid JSON")
+
+                # Status code errors (!= 200) need to be retried since they're caused by stuff like rate limits (429),
+                # but errors caused by 'status' key not being 'success' don't need to be tried because
+                # they're caused by things like private, reserved IP range and invalid query, so we are verifying
+                # either the message key is absent (all necessary data gathered) or exists but doesn't start with
+                # "Status code: " meaning not a status code error so it doesn't need to be retried hence we can safely
+                # retain the already present data and only update the last_seen key
+
+                if (data.get(ip) and not data.get(ip).get("detail")) or (data.get(ip) and data.get(ip).get("detail") and not data.get(ip).get("detail").startswith("Status code: ")):
+                    # Update the last seen only, use ISO 8601 time format
+                    data[ip]["last_seen"] = str(datetime.now(UTC))
+
+                    with open(self.log_path, "w") as f:
+                        f.write(json.dumps(data, indent=4))
+
+                    # Also log even when data is already stored if logging enabled
+                    # Check if message key is absent which implies the request stored in logs has all necessary
+                    # data, so well use that for logging
+                    if self.log_to_console and not data.get(ip).get("detail"):
+                        stored_data = data[ip]
+                        logging.info(f"""
+                            IP: {ip}
+                            Country: {stored_data["country"]}
+                            Continent: {stored_data["continent"]}
+                            Proxy: {stored_data["is_proxy"]}
+                            """)
+
+                    await self.app(scope, receive, send)
+                    return
+
+
+            # If absent, send request
+            async with httpx.AsyncClient() as client:
+                ip = scope["client"][0]
+                response = await client.get(
+                    f"http://ip-api.com/json/{ip}?keys=status,country,continent,city,regionName,proxy,hosting,mobile,reverse")
+                response_json = response.json()
+
+            # Immediately log to console if console logging is enabled
+            if self.log_to_console:
+                if response_json["status"] != "success" or response.status_code != 200:
+                    logging.warning(
+                        f"IP lookup for \'{ip}\' failed, status code: {response.status_code}" if response.status_code != 200 else f"IP lookup for \'{ip}\' failed, \'status\' key wasn't \'success\'")
+                else:
+                    logging.info(f"""
+                    IP: {ip}
+                    Country: {response_json["country"]}
+                    Continent: {response_json["continent"]}
+                    Proxy: {response_json["proxy"]}
+                    """)
+
+            # Log to file and stuff
+            if response_json["status"] != "success" or response.status_code != 200:
+                data[ip] = {
+                    "detail": "IP lookup failed",
+                    "reason": "\'status\' key wasn't \'success\'" if response_json["status"] != "success" else f"Status code: {response.status_code}",
+                    "last_seen": str(datetime.now(UTC))
+                }
+            else:
+                data[ip] = {
+                    "country": response_json["country"],
+                    "continent": response_json["continent"],
+                    "city": response_json["city"],
+                    "region": response_json["regionName"],
+                    "is_proxy": response_json["proxy"],
+                    "is_hosting": response_json["hosting"],
+                    "is_mobile": response_json["mobile"],
+                    "reverseDNS": response_json["reverse"] if response_json["reverse"] else "N/A",
+                    "last_seen": str(datetime.now(UTC))
+                }
+
+            with open(self.log_path, "w") as f:
+                f.write(json.dumps(data, indent=4))
+
+            await self.app(scope, receive, send)
+            return
+
+        # TOML FORMAT HANDLING
+        elif self.file_format == "TOML":
+            # Check if the TOML log file already exists and already stores the data for the IP, if so, only update the time
+            if Path(self.log_path).exists() and Path(self.log_path).is_file():
+                with open(self.log_path, "r") as f:
+                    try:
+                        data = tomllib.loads(f.read())
+                    except Exception:
+                        if f.read():
+                            raise Exception(f"The given log file {self.log_path} does not contain valid TOML")
+
+                # Status code errors (!= 200) need to be retried since they're caused by stuff like rate limits (429),
+                # but errors caused by 'status' key not being 'success' don't need to be tried because
+                # they're caused by things like private, reserved IP range and invalid query, so we are verifying
+                # either the message key is absent (all necessary data gathered) or exists but doesn't start with
+                # "Status code: " meaning not a status code error so it doesn't need to be retried hence we can safely
+                # retain the already present data and only update the last_seen key
+
+                if (data.get(ip) and not data.get(ip).get("detail")) or (data.get(ip) and data.get(ip).get("detail") and not data.get(ip).get("detail").startswith("Status code: ")):
+                    # Update the last seen only, use ISO 8601 time format
+                    data[ip]["last_seen"] = str(datetime.now(UTC))
+
+                    with open(self.log_path, "w") as g:
+                        g.write(tomli_w.dumps(data, indent=4) + "\n")
+
+                    # Also log even when data is already stored if logging enabled
+                    # Check if message key is absent which implies the request stored in logs has all necessary
+                    # data, so well use that for logging
+                    if self.log_to_console and not data.get(ip).get("detail"):
+                        stored_data = data[ip]
+                        logging.info(f"""
+                            IP: {ip}
+                            Country: {stored_data["country"]}
+                            Continent: {stored_data["continent"]}
+                            Proxy: {stored_data["is_proxy"]}
+                            """)
+
+                    await self.app(scope, receive, send)
+                    return
+
+            # If not, send request
+            async with httpx.AsyncClient() as client:
+                ip = scope["client"][0]
+                response = await client.get(
+                    f"http://ip-api.com/json/{ip}?keys=status,country,continent,city,regionName,proxy,hosting,mobile,reverse")
+                response_json = response.json()
+
+            # Immediately log to console if console logging is enabled
+            if self.log_to_console:
+                if response_json["status"] != "success" or response.status_code != 200:
+                    logging.warning(
+                        f"IP lookup for \'{ip}\' failed, status code: {response.status_code}" if response.status_code != 200 else f"IP lookup for \'{ip}\' failed, \'status\' key wasn't \'success\'")
+                else:
+                    logging.info(f"""
+                    IP: {ip}
+                    Country: {response_json["country"]}
+                    Continent: {response_json["continent"]}
+                    Proxy: {response_json["proxy"]}
+                    """)
+
+
+            # Log to file and stuff
+            if response_json["status"] != "success" or response.status_code != 200:
+                data[ip]  = {
+                      "detail": "IP lookup failed",
+                      "reason":  "\'status\' key wasn't \'success\'" if response_json["status"] != "success" else f"Status code: {response.status_code}",
+                      "last_seen": str(datetime.now(UTC))
+                  }
+            else:
+                data[ip] = {
+                        "country": response_json["country"],
+                        "continent": response_json["continent"],
+                        "city": response_json["city"],
+                        "region": response_json["regionName"],
+                        "is_proxy": response_json["proxy"],
+                        "is_hosting": response_json["hosting"],
+                        "is_mobile": response_json["mobile"],
+                        "reverseDNS": response_json["reverse"] if response_json["reverse"] else "N/A",
+                        "last_seen": str(datetime.now(UTC))
+                    }
+
+
+            with open(self.log_path, "w") as f:
+                f.write(tomli_w.dumps(data) + "\n")
+
+            await self.app(scope, receive, send)
+            return
+
